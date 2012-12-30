9c538f9fc87cc383201c0932f06635da51dcc8b1
David Marchelya <dmarchelya@bespokeindustries.com>
2012-12-29 20:21:02 -0700

    Updated the assembly info.  Versioned the client as 0.5.0.

--- Source/Bespoke.CloudFlareDnsClient/Properties/AssemblyInfo.cs
+++ Source/Bespoke.CloudFlareDnsClient/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Bespoke.CloudFlareDnsClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bespoke.CloudFlareDnsClient")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Bespoke Industries")]
+[assembly: AssemblyProduct("Bespoke CloudFlare Dns Client")]
+[assembly: AssemblyCopyright("Copyright © Bespoke Industries 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
